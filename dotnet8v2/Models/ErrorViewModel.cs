6287b16eefcd9b614bbe9b4719391f59f6b695a8
tony <tony@TonyWin11pro>
2024-07-18 09:18:45 +0000

    Add project files.

---
+++ dotnet8v2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet8v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
